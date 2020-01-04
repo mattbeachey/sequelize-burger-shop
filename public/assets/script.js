a9971c9809136a61e911c8ddebf76212382984c4
Matt Beachey <mattbeachey@gmail.com>
2020-01-04 05:43:34 -0600

    menu animation

--- public/assets/script.js
+++ public/assets/script.js
@@ -5,6 +5,17 @@
 const burgerInputEl = document.getElementById("burgerName")
 const burgerButtonEl = document.getElementById("addBurger")
 
+//click handler for menu/about block
+const aboutButtonEl = document.getElementById("about-box")
+const topBarEl = document.getElementById("bar1")
+const midBarEl = document.getElementById("bar2")
+const botBarEl = document.getElementById("bar3")
+aboutButtonEl.addEventListener("click", function (){
+    topBarEl.classList.toggle("bar1clicked")
+    midBarEl.classList.toggle("bar2clicked")
+    botBarEl.classList.toggle("bar3clicked")
+})
+
 
 //add a burger
 burgerButtonEl.addEventListener("click", function () {
